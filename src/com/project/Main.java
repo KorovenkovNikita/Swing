70394b3b735f82684ceefbc2e913988134070a49
Korovenkov Nikita <korovenkov.nik@mail.ru>
2022-09-24 18:11:55 +0300

    Add keyStroke

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -9,18 +9,24 @@ import java.awt.event.*;
 public class Main {
     private static final Integer width = 600;
     private static final Integer height = 400;
-    private static  JFrame jFrame = new JFrame();
-    private static  JPanel jPanel = new JPanel();
+    private static JFrame jFrame = new JFrame();
+    private static JPanel jPanel = new JPanel();
+
     public static void main(String[] args) throws Exception {
         getFrame();
     }
 
     static JFrame getFrame() throws Exception {
         jFrame.add(jPanel);
-        JButton jButton = new JButton(new MyAction());
-        jButton.setText("Изменить цвет на синий");
-        jPanel.add(jButton);
-
+        AbstractAction myAction = new MyAction();
+        JButton makeTheBackgroundBlue = new JButton(myAction);
+        makeTheBackgroundBlue.setText("Изменить цвет на синий");
+        jPanel.add(makeTheBackgroundBlue);
+        KeyStroke keyStrokeMakeTheBackgroundBlue = KeyStroke.getKeyStroke("ctrl B");
+        InputMap inputMapMakeTheBackgroundBlue = jPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        inputMapMakeTheBackgroundBlue.put(keyStrokeMakeTheBackgroundBlue, "change color");
+        ActionMap actionMapMakeTheBackgroundBlue = jPanel.getActionMap();
+        actionMapMakeTheBackgroundBlue.put("change color", myAction);
         jFrame.setVisible(true);
         jFrame.setSize(width, height);
         jFrame.setLocationRelativeTo(null);
@@ -29,12 +35,13 @@ public class Main {
     }
 
     static class MyAction extends AbstractAction {
-     public MyAction(){
-            putValue(AbstractAction.SHORT_DESCRIPTION,"My small action");
+        public MyAction() {
+            putValue(AbstractAction.SHORT_DESCRIPTION, "My small action");
         }
+
         @Override
         public void actionPerformed(ActionEvent e) {
-jPanel.setBackground(Color.BLUE);
+            jPanel.setBackground(Color.BLUE);
         }
     }
 }
